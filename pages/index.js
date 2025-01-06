399f8b2ee831fc94ecb33a78ccfe9d3ebb39770a
LukeFalcox <101217187+LukeFalcox@users.noreply.github.com>
2025-01-06 00:42:59 +0000

    adiciona um arquivo `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,4 @@ function Home(params) {
   return <h1>Eae como foi seu final de semana</h1>;
 }
 
-function teste(params) {
-  console.log("teste");
-}
-
-function teste2(params) {
-  console.log("teste");
-}
 export default Home;
