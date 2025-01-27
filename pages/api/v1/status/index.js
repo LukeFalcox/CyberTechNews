8653cc6dbc524c43b1b483d751d9683de59be1ca
LukeFalcox <101217187+LukeFalcox@users.noreply.github.com>
2025-01-27 12:07:31 +0000

    add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as sum");
+  console.log(result.rows);
   return response.status(200).json({ teste: "TESTE TABNEWS OLÁ A TODOS" });
 }
 
