90e141060db7bba1a10493d3085782ed8974a7b1
LukeFalcox <101217187+LukeFalcox@users.noreply.github.com>
2025-01-30 17:11:44 +0000

    finish `/api/v1/status` endpoint

--- tests/intergration/api/v1/status/get.test.js
+++ tests/intergration/api/v1/status/get.test.js
@@ -1,5 +1,14 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  console.log(response);
   expect(response.status).toBe(200);
+
+  const responsebody = await response.json();
+
+  const parseUpdatedAt = new Date(responsebody.updated_at).toISOString();
+
+  expect(responsebody.updated_at).toEqual(parseUpdatedAt);
+
+  expect(responsebody.dependencies.database.version).toEqual("16.0");
+  expect(responsebody.dependencies.database.max_connections).toEqual(100);
+  expect(responsebody.dependencies.database.opened_connections).toEqual(1);
 });
