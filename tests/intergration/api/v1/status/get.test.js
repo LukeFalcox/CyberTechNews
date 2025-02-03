bd2b2303992463e376bb59ddfa9a400d88106f13
LukeFalcox <101217187+LukeFalcox@users.noreply.github.com>
2025-02-03 12:36:41 +0000

    makes `database.js` `cliente.connect()` more robust to errors

--- tests/intergration/api/v1/status/get.test.js
+++ tests/intergration/api/v1/status/get.test.js
@@ -4,6 +4,8 @@ test("GET to /api/v1/status should return 200", async () => {
 
   const responsebody = await response.json();
 
+  console.log(responsebody);
+
   const parseUpdatedAt = new Date(responsebody.updated_at).toISOString();
 
   expect(responsebody.updated_at).toEqual(parseUpdatedAt);
